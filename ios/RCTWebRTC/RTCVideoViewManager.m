2823fc9b6f044e6b72c4403cef7be3d5e259ba40
Daniel Davis <danielbdavis@gmail.com>
2017-02-02 21:05:23 -0800

    Make RTCVideoView public and overridable.

--- ios/RCTWebRTC/RTCVideoViewManager.m
+++ ios/RCTWebRTC/RTCVideoViewManager.m
@@ -8,7 +8,7 @@
 #import <AVFoundation/AVFoundation.h>
 #import <objc/runtime.h>
 
-#import <WebRTC/RTCEAGLVideoView.h>
+
 #import <WebRTC/RTCMediaStream.h>
 #import <WebRTC/RTCVideoFrame.h>
 #import <WebRTC/RTCVideoTrack.h>
@@ -39,37 +39,6 @@ typedef NS_ENUM(NSInteger, RTCVideoViewObjectFit) {
   RTCVideoViewObjectFitCover
 };
 
-/**
- * Implements an equivalent of {@code HTMLVideoElement} i.e. Web's video
- * element.
- */
-@interface RTCVideoView : UIView <RTCVideoRenderer, RTCEAGLVideoViewDelegate>
-
-/**
- * In the fashion of
- * https://www.w3.org/TR/html5/embedded-content-0.html#dom-video-videowidth
- * and https://www.w3.org/TR/html5/rendering.html#video-object-fit, resembles
- * the CSS style {@code object-fit}.
- */
-@property (nonatomic) RTCVideoViewObjectFit objectFit;
-
-/**
- * The {@link RTCEAGLVideoView} which implements the actual
- * {@link RTCVideoRenderer} of this instance and which this instance fits within
- * itself so that the rendered video preserves the aspect ratio of
- * {@link #_videoSize}.
- */
-@property (nonatomic, readonly) RTCEAGLVideoView *subview;
-
-/**
- * The {@link RTCVideoTrack}, if any, which this instance renders.
- */
-@property (nonatomic, strong) RTCVideoTrack *videoTrack;
-
-
-@property (nonatomic, strong) NSNumber* index;
-
-@end
 
 @implementation RTCVideoView {
   /**
